8c5d918a1547245847f78e24b15c457cd76b320d
Krystian Kluba <klubakrystian@gmail.com>
2026-01-08 07:02:02 +0100

    footer

--- src/components/views/Footer/Footer.js
+++ src/components/views/Footer/Footer.js
@@ -1,5 +1,9 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="text-center text-muted m-auto p-3">
+      Copyright © BlogApp 2026
+    </div>
+  );
 };
 
 export default Footer;
