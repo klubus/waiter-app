ba44698437693e22fce78b5bac381e679a072867
Krystian Kluba <klubakrystian@gmail.com>
2026-01-08 07:00:45 +0100

    header

--- src/components/views/NavBar/NavBar.js
+++ src/components/views/NavBar/NavBar.js
@@ -1,5 +1,21 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { NavLink } from 'react-router-dom';
+
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <Navbar bg="primary" data-bs-theme="dark" className="rounded my-3">
+      <Container>
+        <Navbar.Brand href="#home">Waiter.app</Navbar.Brand>
+        <Nav className="ms-auto">
+          <Nav.Link as={NavLink} to="/">
+            Home
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default NavBar;
