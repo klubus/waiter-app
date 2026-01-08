05eb2cc4c2b525eea1a9028d4dfd7f7853825c4b
Krystian Kluba <klubakrystian@gmail.com>
2026-01-08 06:43:42 +0100

    routing - header footer

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import { Routes, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import NotFound from './components/pages/NotFound/NotFound';
 import Home from './components/pages/Home/Home';
+import Header from './components/views/Header/Header';
+import Footer from './components/views/Footer/Footer';
 import SingleTable from './components/pages/SingleTable/SingleTable';
 
 function App() {
   return (
     <Container>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/table/:id" element={<SingleTable />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+      <Footer />
     </Container>
   );
 }
---
+++ src/components/views/Header/Header.js
@@ -0,0 +1,7 @@
+import Navbar from '../NavBar/NavBar';
+
+const Header = () => {
+  return <Navbar />;
+};
+
+export default Header;
